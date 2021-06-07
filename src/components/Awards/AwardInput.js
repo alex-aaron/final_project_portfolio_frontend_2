28a649494b3b37430339aac7bd298395ba41b436
Alex <alexfrancisaaron@gmail.com>
2021-06-07 15:26:12 -0500

    created AwardInput component

---
+++ src/components/Awards/AwardInput.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class AwardInput extends Component {
+
+  constructor(props) {
+    super() 
+      this.state = {
+        title: "",
+        text: ""
+      }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+};
+
+export default AwardInput;
--- src/containers/AwardContainer.js
+++ src/containers/AwardContainer.js
@@ -7,6 +7,7 @@ class AwardContainer extends Component {
   render() {
     return (
       <div>
+          <h1>Award Container Component</h1>
       </div>
     )
   }
