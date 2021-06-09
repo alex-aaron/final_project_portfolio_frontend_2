15ab70cf2d40cd3548681c1ec91ea0a95abf39c7
Alex <alexfrancisaaron@gmail.com>
2021-06-09 15:43:54 -0500

    changed state to remove nested elements

--- src/components/Awards/AwardInput.js
+++ src/components/Awards/AwardInput.js
@@ -7,13 +7,11 @@ class AwardInput extends Component {
       this.state = {
         award: {
           category: "",
-          nominees: {
-            nominee1: "",
-            nominee2: "",
-            nominee3: "",
-            nominee4: "",
-            nominee5: ""
-          },
+          nominee1: "",
+          nominee2: "",
+          nominee3: "",
+          nominee4: "",
+          nominee5: "",
           winner: ""
         }
       }
@@ -25,27 +23,31 @@ handleOnSubmit = event => {
   })
 }
 
+handleOnChange = event => {
+  this.setState({
+
+  })
+}
+
   render() {
 
     return (
       <div>
         <form onYearSubmit={this.handleYearSubmit}>
-          <label>Year: </label>
-          <input type="text"></input>
           <label>Category Name: </label>
-          <input type="text"></input><br></br>
+          <input type="text" id="category"></input><br></br>
           <label>Nominee 1:</label>
-          <input type="text"></input><br></br>
+          <input type="text" id="nominee1"></input><br></br>
           <label>Nominee 2:</label>
-          <input type="text"></input><br></br>
+          <input type="text" id="nominee2"></input><br></br>
           <label>Nominee 3:</label>
-          <input type="text"></input><br></br>
+          <input type="text" id="nominee3"></input><br></br>
           <label>Nominee 4:</label>
-          <input type="text"></input><br></br>
+          <input type="text" id="nominee4"></input><br></br>
           <label>Nominee 5:</label>
-          <input type="text"></input><br></br>
-          <label>Nominee 5:</label>
-          <input type="text"></input><br></br>
+          <input type="text" id="nominee5"></input><br></br>
+          <label>Winner:</label>
+          <input type="text" id="winner"></input><br></br>
           <button type="submit">Submit</button>
         </form>
       </div>
